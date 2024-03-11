b52474f40e5081ca4884ef695ef8c089a6b99ec3
SirenityK <eldarielmario@gmail.com>
2024-03-11 11:26:39 -0700

    First design patterns

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,15 @@
-<script>
+<script lang="ts">
 	import '../app.pcss';
 	import { ModeWatcher } from 'mode-watcher';
 	import * as Menubar from '$lib/components/ui/menubar/index';
 	import { toggleMode } from 'mode-watcher';
+	import Sun from 'lucide-svelte/icons/sun';
+	import { Button } from '$lib/components/ui/button';
+	import Moon from 'lucide-svelte/icons/moon';
+
+	const gitref = (repo: string) => {
+		return `https://github.com/SirenityK/${repo}`;
+	};
 </script>
 
 <ModeWatcher />
@@ -11,27 +18,50 @@
 		<Menubar.Menu>
 			<Menubar.Trigger>Github</Menubar.Trigger>
 			<Menubar.Content>
-				<Menubar.Item>Jason</Menubar.Item>
+				<Menubar.Item href={gitref('jason-voice-ai')}>Jason</Menubar.Item>
 				<Menubar.Sub>
 					<Menubar.SubTrigger>Boring Calculator (private)</Menubar.SubTrigger>
 					<Menubar.SubContent>
-						<Menubar.Item>Boring Calculator</Menubar.Item>
-						<Menubar.Item>Android Port</Menubar.Item>
-						<Menubar.Item>old calculator</Menubar.Item>
+						<Menubar.Item href={gitref('boringcalculator')}>Boring Calculator</Menubar.Item>
+						<Menubar.Item href={gitref('android-boring-calculator')}>Android Port</Menubar.Item>
+						<Menubar.Item href={gitref('old-calculator')}>old calculator</Menubar.Item>
 					</Menubar.SubContent>
 				</Menubar.Sub>
 				<Menubar.MenubarSeparator />
-				<Menubar.Item>Trash-game</Menubar.Item>
+				<Menubar.Item href={gitref('trash-game')}>Trash game</Menubar.Item>
 			</Menubar.Content>
+		</Menubar.Menu>
+
+		<Menubar.Menu>
+			<Menubar.Trigger>Posts</Menubar.Trigger>
+		</Menubar.Menu>
+
+		<Menubar.Menu>
+			<Menubar.Trigger>Curriculum</Menubar.Trigger>
 		</Menubar.Menu>
 	</Menubar.Root>
 	<div class="ml-auto grid">
-		<button on:click={toggleMode}>Toggle theme</button>
+		<!-- <button on:click={toggleMode}>Toggle theme</button> -->
+		<Button on:click={toggleMode} variant="outline" size="icon">
+			<Sun
+				class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+			/>
+			<Moon
+				class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+			/>
+			<span class="sr-only">Toggle theme</span>
+		</Button>
 	</div>
 </header>
 <slot />
 
 <style lang="scss">
+	:global(h1 h2 p span) {
+		font-family: 'Inter', sans-serif;
+		font-optical-sizing: auto;
+		font-style: normal;
+		font-variation-settings: 'slnt' 0;
+	}
 	:global(body) {
 		@apply mx-5 mt-5;
 	}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,8 @@
 <script lang="ts">
-	const name = 'Dariel Fierro';
-	const nlength = name.length;
-	let count = 1;
-	let displayName: string = '';
-
-	const interval = setInterval(() => {
-		displayName = name.slice(0, count);
-		count++;
-		if (count > nlength) {
-			clearInterval(interval);
-		}
-	}, 100);
+	import TextEffect from './textEffect.svelte';
 </script>
 
 <main class="mt-5">
-	<h1 class="text-2xl">{displayName}</h1>
+	<TextEffect text={'Dariel Fierro'} />
+	<TextEffect text={'Estudiante, ingeniero de software'} fonts="defaults" />
 </main>

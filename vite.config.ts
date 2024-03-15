90ef92199f425575641cac3a5055b28b7de64324
SirenityK <eldarielmario@gmail.com>
2024-03-15 08:55:02 -0700

    add package-lock and remove svg plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import svg from '@poppanator/sveltekit-svg';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), enhancedImages(), svg()]
+	plugins: [sveltekit(), enhancedImages()]
 });
